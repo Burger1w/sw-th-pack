75802103fa45f7c2d5a0ec9e853d45422ec966bf
Burger1w <104843190+Burger1w@users.noreply.github.com>
2022-10-09 17:15:32 +0300

    Update activity.jsx

--- pages/api/util/activity.jsx
+++ pages/api/util/activity.jsx
@@ -2,6 +2,6 @@ import fetch from 'isomorphic-unfetch';
 
 export default async function Activity(req, res) {
     res.json(
-        await (await fetch('https://linkcord.js.org/api/v3/user/451444721089380373')).json()
+        await (await fetch('https://linkcord.js.org/api/v3/user/1005112531431469136')).json()
     );
 };
